67cac2087e8f163682dd808fd583130d600595bd
Vladimir Krasniy <krasniy.vladim@yandex.ru>
2022-03-27 00:21:25 +0300

    Binary-Tree-Search

---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace ConsoleApp48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + ", ");
+            }
+            Console.ReadLine();
+
+            Stopwatch();
+
+            foreach (var queue in tree.LevelOrderTraversal())
+            {
+                Console.Write(queue + ", ");
+            }
+            Console.ReadLine();
+
+            Stopwatch();
+        }
+
+        static void Stopwatch()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Thread.Sleep(10000);
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+    }
+
+    class Node<T> : IComparable<T>, IComparable
+        where T : IComparable
+    {
+        public T Data { get; private set; }
+
+        public Node<T> Left { get; private set; }
+
+        public Node<T> Right { get; private set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new Exception("не совпадает");
+            }
+        }
+
+        public int CompareTo(T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+
+    class Tree<T>
+        where T : IComparable
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            if (Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+
+        }
+
+        public List<T> Preorder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return Preorder(Root);
+        }
+        private List<T> Preorder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                list.Add(node.Data);
+                if (node.Left != null)
+                {
+                    list.AddRange(Preorder(node.Left));
+                }
+                if (node.Right != null)
+                {
+                    list.AddRange(Preorder(node.Right));
+                }
+            }
+
+            return list;
+        }
+
+        public IEnumerable<Node<T>> LevelOrderTraversal()
+        {
+
+            if (Root == null)
+                yield break;
+
+            var queue = new Queue<Node<T>>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                yield return node;
+
+                if (node.Left != null)
+                    queue.Enqueue(node.Left);
+
+                if (node.Right != null)
+                    queue.Enqueue(node.Right);
+            }
+        }
+    }
+}
